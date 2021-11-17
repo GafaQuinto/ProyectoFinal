c4dfb3e7dac52ba38391d77e639e21787dbae17c
ZUMBADO HERRERA <gabriel.zumbado1@ulatina.net>
2021-11-16 20:37:08 -0600

    Gafa

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,69 +22,99 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button4 = New System.Windows.Forms.Button()
+        Me.btIngresar = New System.Windows.Forms.Button()
+        Me.btRegistrarse = New System.Windows.Forms.Button()
+        Me.lbRegistrar = New System.Windows.Forms.Label()
+        Me.txtUsuario = New System.Windows.Forms.TextBox()
+        Me.txtContraseña = New System.Windows.Forms.TextBox()
+        Me.lbUsuario = New System.Windows.Forms.Label()
+        Me.lbContraseña = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'Button1
+        'btIngresar
         '
-        Me.Button1.Location = New System.Drawing.Point(640, 533)
-        Me.Button1.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(150, 44)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btIngresar.Location = New System.Drawing.Point(623, 409)
+        Me.btIngresar.Margin = New System.Windows.Forms.Padding(6)
+        Me.btIngresar.Name = "btIngresar"
+        Me.btIngresar.Size = New System.Drawing.Size(169, 44)
+        Me.btIngresar.TabIndex = 2
+        Me.btIngresar.Text = "Ingresar" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.btIngresar.UseVisualStyleBackColor = True
         '
-        'Button2
+        'btRegistrarse
         '
-        Me.Button2.Location = New System.Drawing.Point(640, 446)
-        Me.Button2.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(150, 44)
-        Me.Button2.TabIndex = 1
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btRegistrarse.Location = New System.Drawing.Point(623, 563)
+        Me.btRegistrarse.Name = "btRegistrarse"
+        Me.btRegistrarse.Size = New System.Drawing.Size(169, 43)
+        Me.btRegistrarse.TabIndex = 3
+        Me.btRegistrarse.Text = "Registrarse "
+        Me.btRegistrarse.UseVisualStyleBackColor = True
         '
-        'Button3
+        'lbRegistrar
         '
-        Me.Button3.Location = New System.Drawing.Point(640, 373)
-        Me.Button3.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(150, 44)
-        Me.Button3.TabIndex = 2
-        Me.Button3.Text = "Button3"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.lbRegistrar.AutoSize = True
+        Me.lbRegistrar.Location = New System.Drawing.Point(585, 524)
+        Me.lbRegistrar.Name = "lbRegistrar"
+        Me.lbRegistrar.Size = New System.Drawing.Size(253, 25)
+        Me.lbRegistrar.TabIndex = 4
+        Me.lbRegistrar.Text = "¿Aún no está registrado?"
         '
-        'Button4
+        'txtUsuario
         '
-        Me.Button4.Location = New System.Drawing.Point(623, 610)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(185, 43)
-        Me.Button4.TabIndex = 3
-        Me.Button4.Text = "Button4"
-        Me.Button4.UseVisualStyleBackColor = True
+        Me.txtUsuario.Location = New System.Drawing.Point(570, 242)
+        Me.txtUsuario.Name = "txtUsuario"
+        Me.txtUsuario.Size = New System.Drawing.Size(284, 31)
+        Me.txtUsuario.TabIndex = 5
+        '
+        'txtContraseña
+        '
+        Me.txtContraseña.Location = New System.Drawing.Point(570, 345)
+        Me.txtContraseña.Name = "txtContraseña"
+        Me.txtContraseña.Size = New System.Drawing.Size(284, 31)
+        Me.txtContraseña.TabIndex = 6
+        '
+        'lbUsuario
+        '
+        Me.lbUsuario.AutoSize = True
+        Me.lbUsuario.Location = New System.Drawing.Point(667, 197)
+        Me.lbUsuario.Name = "lbUsuario"
+        Me.lbUsuario.Size = New System.Drawing.Size(86, 25)
+        Me.lbUsuario.TabIndex = 7
+        Me.lbUsuario.Text = "Usuario"
+        '
+        'lbContraseña
+        '
+        Me.lbContraseña.AutoSize = True
+        Me.lbContraseña.Location = New System.Drawing.Point(646, 298)
+        Me.lbContraseña.Name = "lbContraseña"
+        Me.lbContraseña.Size = New System.Drawing.Size(123, 25)
+        Me.lbContraseña.TabIndex = 8
+        Me.lbContraseña.Text = "Contraseña"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1600, 865)
-        Me.Controls.Add(Me.Button4)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
-        Me.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
+        Me.Controls.Add(Me.lbContraseña)
+        Me.Controls.Add(Me.lbUsuario)
+        Me.Controls.Add(Me.txtContraseña)
+        Me.Controls.Add(Me.txtUsuario)
+        Me.Controls.Add(Me.lbRegistrar)
+        Me.Controls.Add(Me.btRegistrarse)
+        Me.Controls.Add(Me.btIngresar)
+        Me.Margin = New System.Windows.Forms.Padding(6)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Button4 As Button
+    Friend WithEvents btIngresar As Button
+    Friend WithEvents btRegistrarse As Button
+    Friend WithEvents lbRegistrar As Label
+    Friend WithEvents txtUsuario As TextBox
+    Friend WithEvents txtContraseña As TextBox
+    Friend WithEvents lbUsuario As Label
+    Friend WithEvents lbContraseña As Label
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,5 +1,8 @@
 ﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles btRegistrarse.Click
+        Form2.Show()
     End Sub
+
 End Class
